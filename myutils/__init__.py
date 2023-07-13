01ef722daba762e766313ec19ab535e95fab3697
yilintung <yilintung@protonmail.com>
2023-07-13 20:05:09 +0800

    將常用程式碼移至套件中

--- myutils/__init__.py
+++ myutils/__init__.py
@@ -3,6 +3,8 @@
 #
 
 import myutils.TurningPoints
+import myutils.DirectionalChange
 
 
 FindingTurningPoints = TurningPoints.FindingTurningPoints
+FindingDirectionalChangePoints = DirectionalChange.FindingDirectionalChangePoints
