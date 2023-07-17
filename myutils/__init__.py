460beb7d065d85ef5674670791d2a5003c95d50a
yilintung <yilintung@protonmail.com>
2023-07-17 14:57:46 +0800

    Update __init__.py

--- myutils/__init__.py
+++ myutils/__init__.py
@@ -5,8 +5,10 @@
 import myutils.TurningPoints
 import myutils.DirectionalChange
 import myutils.PerceptuallyImportant
+import myutils.RankEvaluate
 
 
 FindingTurningPoints = TurningPoints.FindingTurningPoints
 FindingDirectionalChangePoints = DirectionalChange.FindingDirectionalChangePoints
 FindingPerceptuallyImportantPoints = PerceptuallyImportant.FindingPerceptuallyImportantPoints
+StockRankEvaluate = RankEvaluate.StockRankEvaluate
