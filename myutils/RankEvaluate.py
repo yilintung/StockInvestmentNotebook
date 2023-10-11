cd0d71006da43411bb16029ffd0a4f2bd7f9af59
yilintung <yilintung@protonmail.com>
2023-10-11 10:31:21 +0800

    Update RankEvaluate.py

--- myutils/RankEvaluate.py
+++ myutils/RankEvaluate.py
@@ -9,11 +9,12 @@ import numpy as np
 class StockRankEvaluate :
     
     ##### StockRankEvaluate類別的建構子 #####
-    # base_prices : 基準價格，一般使用收盤價 (numpy.ndarray)
+    # base_prices : 基準價格，一般使用收盤價 (型態為numpy.ndarray)
     def __init__( self, base_prices) :
         if base_prices is not None and type(base_prices) is np.ndarray :
             self.base_prices_mean = np.mean(base_prices)
             self.base_prices_std  = np.std(base_prices)
+            # 除錯訊息
             print("基準價格區間 ： 平均值 = {:.2f} ， 標準差 ＝ {:.2f} ， 最高 ＝ {:.2f} ， 最低 ＝ {:.2f}".format(self.base_prices_mean,self.base_prices_std,np.max(base_prices),np.min(base_prices)))
         else :
             raise ValueError
@@ -23,17 +24,16 @@ class StockRankEvaluate :
     def eval( self, price) :
         rank = ''
         price_std_range = (price - self.base_prices_mean) / self.base_prices_std
-        if price_std_range < -2.0 :
+        if price_std_range <= -2.0 :
             rank = '極低'
         elif price_std_range > -2.0 and price_std_range < -1.0 :
             rank = '低'
-        elif price_std_range > -1.0 and price_std_range <= 0 :
-            rank = '稍低'
-        elif price_std_range > 0 and price_std_range < 1.0 :
-            rank = '稍高'
+        elif price_std_range >= -1.0 and price_std_range <= 1.0 :
+            rank = '中'
         elif price_std_range > 1.0 and price_std_range < 2.0 :
             rank = '高'
-        elif price_std_range > 2.0 :
+        elif price_std_range >= 2.0 :
             rank = '極高'
+        # 除錯訊息
         print("價格 ＝ {:.2f}元（標準差範圍 ＝ {:.2f}） ， 位階評價：{}".format(price,price_std_range,rank))
         return rank
